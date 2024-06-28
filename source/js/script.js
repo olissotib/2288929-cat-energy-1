476a98e6caef914caade2b70164a41ced7c84eb0
Olis Sotib <119186121+olissotib@users.noreply.github.com>
2024-06-28 18:44:28 +0300

    Исправляет js

--- source/js/script.js
+++ source/js/script.js
@@ -1,17 +1,13 @@
 navigationMain.classList.remove('header-index__navigation--nojs');
+let navigationMain = document.querySelector('.header-index__navigation');
+let navigationButton = document.querySelector('.header-index__navigation-button');
 
-const menuOpener = () => {
-  let navigationMain = document.querySelector('.header-index__navigation');
-  let navigationButton = document.querySelector('.header-index__navigation-button');
-  navigationButton.addEventListener('click', function () {
-    if (navigationMain.classList.contains('header-index__navigation--closed')) {
-      navigationMain.classList.remove('header-index__navigation--closed');
-      navigationMain.classList.add('header-index__navigation--opened');
-    } else {
-    navigationMain.classList.add('header-index__navigation--closed');
-    navigationMain.classList.remove('header-index__navigation--opened');
-    }
-  })
-}
-
-menuOpener();
+navigationButton.addEventListener('click', function () {
+  if (navigationMain.classList.contains('header-index__navigation--closed')) {
+    navigationMain.classList.remove('header-index__navigation--closed');
+    navigationMain.classList.add('header-index__navigation--opened');
+  } else {
+  navigationMain.classList.add('header-index__navigation--closed');
+  navigationMain.classList.remove('header-index__navigation--opened');
+  }
+})
